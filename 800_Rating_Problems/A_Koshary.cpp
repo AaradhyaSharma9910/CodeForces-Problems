08bcdda6645edcb547f057c2ab4c861be122f0ad
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-06-17 10:38:02 +0530

    added final changes

--- 800_Rating_Problems/A_Koshary.cpp
+++ 800_Rating_Problems/A_Koshary.cpp
@@ -1,22 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
+void solve() {
+    
+}
+ 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+ 
     int t;
     cin >> t;
-
+ 
     while (t--) {
-        int x, y;
-        cin >> x >> y;
-
-        if (x % 2 == 1 && y % 2 == 1)
-            cout << "NO\n";
-        else
-            cout << "YES\n";
+        long long x,y;
+        cin>>x>>y;
+        long long cal = max(x,y);
+        long long gg = min(x,y);
+        if(cal%2==0 && gg%2==0){
+            cout<<"YES"<<endl;
+        }else if(cal%2==0 && gg%2 == 1 || cal%2 == 1 && gg%2 == 0){
+            cout<<"YES"<<endl;
+        }else{
+            cout<<"NO"<<endl;
+        
+        }
+        solve();
     }
-
+ 
     return 0;
 }
