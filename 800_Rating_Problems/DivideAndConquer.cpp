f2f0f58e75d732180090f6febf3e320944f81176
AaradhyaSharma9910 <aaradhyasharma1622@gmail.com>
2026-07-27 22:02:11 +0530

    added problem rated 800

---
+++ 800_Rating_Problems/DivideAndConquer.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long x,y;
+    cin>>x>>y;
+    if(x%y==0){
+        cout<<"YES"<<endl;
+    }
+    else
+    cout<<"NO"<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
